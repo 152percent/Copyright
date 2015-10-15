82b9f9dd1c7e3c6b693724c891129ea733269d9f
Shaps Mohsenin <shapsuk@me.com>
2015-10-15 22:33:05 +0100

    Initial Commit

---
+++ Copyright/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Copyright
+//
+//  Created by Shaps Mohsenin on 15/10/2015.
+//  Copyright © 2015 Shaps Mohsenin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+

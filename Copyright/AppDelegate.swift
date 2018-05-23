bc7e0458c80357178da16fb32a85311c6e2c32a3
Shaps Benkau <shapsuk@me.com>
2018-05-23 21:47:39 +0100

    Initial Commit

---
+++ Copyright/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Copyright
+//
+//  Created by Shaps Benkau on 23/05/2018.
+//  Copyright © 2018 152percent Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Copyright/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Copyright
+//
+//  Created by Shaps Benkau on 23/05/2018.
+//  Copyright © 2018 152percent Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

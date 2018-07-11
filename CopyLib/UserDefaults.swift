5107dab1620861871f839b18511b7b58fecaf2a6
Shaps Benkau <shapsuk@me.com>
2018-07-11 15:48:39 +0100

    Minor addition

--- CopyLib/UserDefaults.swift
+++ CopyLib/UserDefaults.swift
@@ -14,11 +14,19 @@
      
 import Foundation
 
-extension UserDefaults {
+#if os(iOS)
+import UIKit
+public typealias SystemColor = UIColor
+#else
+import Cocoa
+public typealias SystemColor = NSColor
+#endif
 
-    public func register(defaults: [Key: Any]) {
+public extension UserDefaults {
+
+    func register(defaults: [Key: Any]) {
         let mapped = Dictionary(uniqueKeysWithValues: defaults.map { (key, value) -> (String, Any) in
-            if let color = value as? NSColor {
+            if let color = value as? SystemColor {
                 return (key.rawValue, NSKeyedArchiver.archivedData(withRootObject: color))
             } else {
                 return (key.rawValue, value)
@@ -28,106 +36,86 @@ extension UserDefaults {
         register(defaults: mapped)
     }
 
-    public func set(_ value: Any?, forKey key: Key) {
+    func set(_ value: Any?, forKey key: Key) {
         set(value, forKey: key.rawValue)
     }
 
-    public func set(_ color: NSColor, forKey key: Key) {
+    func set(_ color: SystemColor, forKey key: Key) {
         let data = NSKeyedArchiver.archivedData(withRootObject: color)
         set(data, forKey: key.rawValue)
     }
 
-    public func bool(forKey key: Key) -> Bool {
+    func bool(forKey key: Key) -> Bool {
         return bool(forKey: key.rawValue)
     }
 
-    public func integer(forKey key: Key) -> Int {
+    func integer(forKey key: Key) -> Int {
         return integer(forKey: key.rawValue)
     }
 
-    public func float(forKey key: Key) -> Float {
+    func float(forKey key: Key) -> Float {
         return float(forKey: key.rawValue)
     }
 
-    public func float(forKey key: Key) -> CGFloat {
+    func float(forKey key: Key) -> CGFloat {
         return CGFloat(float(forKey: key) as Float)
     }
 
-    public func double(forKey key: Key) -> Double {
+    func double(forKey key: Key) -> Double {
         return double(forKey: key.rawValue)
     }
 
-    public func url(forKey key: Key) -> URL? {
+    func url(forKey key: Key) -> URL? {
         return url(forKey: key.rawValue)
     }
 
-    public func date(forKey key: Key) -> Date? {
+    func date(forKey key: Key) -> Date? {
         return object(forKey: key.rawValue) as? Date
     }
 
-    public func string(forKey key: Key) -> String? {
+    func string(forKey key: Key) -> String? {
         return string(forKey: key.rawValue)
     }
 
-    public func color(forKey key: Key) -> NSColor? {
+    func color(forKey key: Key) -> SystemColor? {
         return data(forKey: key.rawValue)
-            .flatMap { NSUnarchiver.unarchiveObject(with: $0) as? NSColor }
+            .flatMap { NSUnarchiver.unarchiveObject(with: $0) as? SystemColor }
     }
 
-    public func value<T>(forKey key: Key) -> T? {
+    func value<T>(forKey key: Key) -> T? {
         return value(forKey: key.rawValue) as? T
     }
 
 }
 
-extension UserDefaults {
+public extension UserDefaults {
 
-    public subscript(key: Key) -> Bool {
+    subscript(key: Key) -> Bool {
         get { return bool(forKey: key) }
         set { set(newValue, forKey: key.rawValue) }
     }
 
-    public subscript(key: Key) -> Int {
+    subscript(key: Key) -> Int {
         get { return integer(forKey: key) }
         set { set(newValue, forKey: key.rawValue) }
     }
 
-    public subscript(key: Key) -> Double {
+    subscript(key: Key) -> Double {
         get { return double(forKey: key) }
         set { set(newValue, forKey: key.rawValue) }
     }
 
-    public subscript(key: Key) -> Float {
+    subscript(key: Key) -> Float {
         get { return float(forKey: key) }
         set { set(newValue, forKey: key.rawValue) }
     }
 
-    public subscript(key: Key) -> CGFloat {
+    subscript(key: Key) -> CGFloat {
         get { return CGFloat(float(forKey: key) as Float) }
         set { set(newValue, forKey: key.rawValue) }
     }
 
-    public subscript(key: Key) -> String? {
-        get { return string(forKey: key) }
-        set { set(newValue, forKey: key.rawValue) }
-    }
-
-    public subscript(key: Key) -> URL? {
-        get { return url(forKey: key) }
-        set { set(newValue, forKey: key.rawValue) }
-    }
-
-    public subscript(key: Key) -> Date? {
-        get { return date(forKey: key) }
-        set { set(newValue, forKey: key.rawValue) }
-    }
-
-    public subscript(key: Key) -> NSColor? {
-        get { return color(forKey: key) }
-        set { set(newValue, forKey: key) }
-    }
-
-    public subscript<T>(key: Key) -> T? {
+    subscript<T>(key: Key) -> T? {
         get { return value(forKey: key) }
         set { set(newValue, forKey: key.rawValue) }
     }
